e1729c3cdabdb25247d4d00a5f65466e72573140
nininv <nininv45@gmail.com>
2021-07-14 14:58:43 +0300

    fix issue of getting machine tags function

---
+++ database/migrations/2021_07_14_051258_add_router_status_to_device_configurations.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRouterStatusToDeviceConfigurations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('device_configurations', function (Blueprint $table) {
+            $table->boolean('router_status')->nullable();
+            $table->unsignedBigInteger('router_update_time')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('device_configurations', function (Blueprint $table) {
+            $table->dropColumn('router_status');
+            $table->dropColumn('router_update_time');
+        });
+    }
+}
